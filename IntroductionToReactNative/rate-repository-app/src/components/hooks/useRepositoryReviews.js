0e7be1661a7b84082593576c85ae9cf8d80c99fb
Daniel <daniel.kortesmaa@edu.turkuamk.fi>
2025-04-23 16:34:23 +0300

    Fixed duplicate children issue

--- IntroductionToReactNative/rate-repository-app/src/components/hooks/useRepositoryReviews.js
+++ IntroductionToReactNative/rate-repository-app/src/components/hooks/useRepositoryReviews.js
@@ -19,6 +19,31 @@ const useRepositoryReviews = (variables) => {
                     after: pageInfo.endCursor,
                     ...variables,
                 },
+                //Prevents duplicate key issue that popped up on later debugging, this is sadly a post submission change since I didnt initially catch it
+                updateQuery: (previousResult, { fetchMoreResult }) => {
+                    if (!fetchMoreResult) {
+                        return previousResult;
+                    }
+
+                    return {
+                        repository: {
+                            ...fetchMoreResult.repository,
+                            reviews: {
+                                ...fetchMoreResult.repository.reviews,
+                                edges: [
+                                    ...previousResult.repository.reviews.edges,
+                                    ...fetchMoreResult.repository.reviews.edges.filter(
+                                        (newEdge) =>
+                                            !previousResult.repository.reviews.edges.some(
+                                                (existingEdge) =>
+                                                    existingEdge.node.id === newEdge.node.id,
+                                            ),
+                                    ),
+                                ],
+                            },
+                        },
+                    };
+                },
             });
         }
     };
